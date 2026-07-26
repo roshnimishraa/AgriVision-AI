fc74e0e37ace1bd7290e59d4e94b061480a0af11
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2026-07-26 21:11:30 +0530

    Update html_utils.py

--- frontend/src/ui/html_utils.py
+++ frontend/src/ui/html_utils.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 
 def render_html(html: str):
- 
+
     flattened = "\n".join(line.strip() for line in html.strip().splitlines())
     st.markdown(flattened, unsafe_allow_html=True)
