572c56b16f051bddb74605c8bfd2b967ebbbe47c
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2026-07-30 12:41:42 +0530

    Delete frontend directory

--- frontend/src/ui/html_utils.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-
-def render_html(html: str):
-
-    flattened = "\n".join(line.strip() for line in html.strip().splitlines())
-    st.markdown(flattened, unsafe_allow_html=True)
